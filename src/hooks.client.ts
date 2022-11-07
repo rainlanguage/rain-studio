e9eb2e26d9e5beeeba41ef7726ce84b57a510882
Josh Hardy <jks.hardy@gmail.com>
2022-11-07 15:21:30 +0000

    debug

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,1 +1,5 @@
+import { env } from '$env/dynamic/public'
+
 import '$lib/supabaseClient'
+
+console.log('client', env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,4 @@
+import { env } from '$env/dynamic/public'
 import '$lib/supabaseClient'
+
+console.log('server', env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
