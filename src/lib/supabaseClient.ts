d6ede7fae4c628d1afed32f803b66b29c893607d
Josh Hardy <jks.hardy@gmail.com>
2022-11-07 17:27:12 +0000

    seems to be working now

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,3 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { env } from '$env/dynamic/public'
 
 export const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY)
