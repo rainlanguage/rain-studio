418397cb5c1f9cd08f1cb30eeec629a463ed7b7b
NanezX <victor.nanezj@gmail.com>
2022-11-19 01:57:02 -0400

    Profile update to link address

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,10 +1,73 @@
-
-import { getServerSession } from '@supabase/auth-helpers-sveltekit';
-import { redirect } from '@sveltejs/kit';
+import { getServerSession, getSupabase } from '@supabase/auth-helpers-sveltekit';
+import { redirect, error, invalid } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load(event) {
-    const session = await getServerSession(event)
-    if (session) return { session }
-    throw redirect(307, '/login')
+	const session = await getServerSession(event);
+	if (session) return { session };
+	throw redirect(307, '/login');
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	getNonce: async (event) => {
+		const { request } = event;
+		const { supabaseClient } = await getSupabase(event);
+
+		const formData = await request.formData();
+		const address = formData.get('address');
+		const { data, error } = await supabaseClient.functions.invoke('get_nonce', {
+			body: { address }
+		});
+
+		return { success: true, id: data.id, error };
+	},
+
+	linkAddress: async (event) => {
+		const { request } = event;
+		const { supabaseClient, session } = await getSupabase(event);
+
+		const formData = await request.formData();
+		const address = formData.get('address');
+		const userId = session.user.id;
+
+		const { data, error: nonceError } = await supabaseClient.functions.invoke('get_nonce', {
+			body: { address }
+		});
+
+		if (nonceError) {
+			// Print on server console
+			const message = `There was an error with the address. Please, check if the address '${address}' is already linked.`;
+			console.log(message);
+
+			return invalid(400, {
+				success: false,
+				error: {
+					message
+				}
+			});
+		}
+		const insertData = {
+			id: data.id,
+			address,
+			user_id: userId
+		};
+
+		const { error: insertError } = await supabaseClient.from('wallets').insert(insertData);
+
+		if (insertError) {
+			// Print on server console
+			const message = `There was an error inserting the address. Please, check if the address '${address}' is already linked.`;
+			console.log(message);
+
+			return invalid(400, {
+				success: false,
+				error: {
+					message
+				}
+			});
+		}
+
+		return { success: true };
+	}
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,116 +1,126 @@
-
 <script lang="ts">
+	import { providers } from 'ethers';
+	import { onMount } from 'svelte';
+	import { Button } from 'rain-svelte-components/package';
+	import { defaultEvmStores, signerAddress, connected } from 'svelte-ethers-store';
+	import Web3Modal from 'web3modal';
+	import WalletConnectProvider from '@walletconnect/web3-provider/dist/umd/index.min';
+
 	import { page } from '$app/stores';
+	import { enhance, applyAction } from '$app/forms';
 	import Account from '$lib/Account.svelte';
 	import Background from '$lib/Background.svelte';
-	import { Button } from 'rain-svelte-components/package';
 	import { supabaseClient } from '$lib/supabaseClient';
 
+	let web3Modal: Web3Modal;
+	let ethersProvider: providers.Web3Provider;
 
-	import { v4 as uuidv4 } from 'uuid';
-
-
-  import { defaultEvmStores, signerAddress } from "svelte-ethers-store";
-	
-	import { afterUpdate } from 'svelte';
-	
-  import { providers  } from "ethers";
-	import Web3Modal from 'web3modal';
-	import WalletConnectProvider  from "@walletconnect/web3-provider/dist/umd/index.min";
-
-	let web3Modal: any;
-	let ethersProvider: any;
+	let wallets: any[] = [];
+	let loading = false;
+	let message = '';
 
 	const providerOptions = {
 		injected: {
 			display: {
-				name: "Metamask",
-				description: "Connect with the provider in your Browser"
+				name: 'Metamask',
+				description: 'Connect with the provider in your Browser'
 			},
 			package: null
 		},
 		walletconnect: {
 			package: WalletConnectProvider,
 			options: {
-				infuraId: "0f270373e0934beda174c537257386b0",
+				infuraId: '0f270373e0934beda174c537257386b0'
 			}
-		},
+		}
 	};
 
-	afterUpdate(() => {
+	onMount(() => {
 		web3Modal = new Web3Modal({
-			cacheProvider: false,
-			providerOptions,
+			cacheProvider: true,
+			providerOptions
 		});
-
-	})
-
+	});
 
 	const connectWallet = async () => {
-		await web3Modal.clearCachedProvider();
-
-    const provider = await web3Modal.connect();
-    ethersProvider = new providers.Web3Provider(provider);
+		const provider = await web3Modal.connect();
+		ethersProvider = new providers.Web3Provider(provider);
 		defaultEvmStores.setProvider(provider);
-		const network = await ethersProvider.getNetwork();
-		// console.log(network);
-  };
+		searchLinkAddress();
+	};
 
 	const getWallets = async () => {
 		const user = $page.data.session.user;
+	};
 
-		const { data, error, status } = await supabaseClient
+	const searchLinkAddress = async () => {
+		loading = true;
+		const user = $page.data.session.user;
+
+		let { data, error } = await supabaseClient
 			.from('profiles')
-			.select(`username, full_name, website, avatar_url, wallets(*)`)
+			.select('wallets(*)')
 			.eq('id', user.id)
 			.single();
 
-			console.log(data);
-	};
-
-	const linkAddress = async () => {
-		const user = $page.data.session.user;
-
-		const updates = {
-			id: uuidv4(),
-			address: $signerAddress,
-			user_id: user.id
-		};
-		
-		let {error}= await supabaseClient.from('wallets').insert(updates);
+		const value = data?.wallets.find((element: any) => {
+			if (element.address == $signerAddress) {
+				return true;
+			}
+		});
 
 		if (error) {
-			console.log("=== Link address error: ")
-			console.log(error)
-		};
+			message = error.message;
+		} else if (value) {
+			message = 'This address is linked to your account.';
+		} else {
+			message = "You don't have address linked to your account.";
+		}
+
+		loading = false;
+	};
+
+	$: if ($connected) {
+		searchLinkAddress();
 	}
-
-
 </script>
+
 <Background>
 	<Account session={$page.data.session} />
+
 	<div class="flex flex-col w-max bg-white p-6 rounded-lg shadow-md space-y-3 ml-5">
 		{#if !$signerAddress}
-			<Button disabled={!!$signerAddress} on:click={connectWallet} >
-				Connect wallet
-			</Button>
-    {:else}
-		<div class="space-y-2">
-			<Button variant="primary" on:click={linkAddress} >
-				Link address
-			</Button>
-			<Button variant="primary" on:click={getWallets} >
-				See addressess
-			</Button>
-			<Button disabled={!$signerAddress} on:click={async () => defaultEvmStores.disconnect()} >
-				Disconnect
-			</Button>
+			<Button disabled={!!$signerAddress} on:click={connectWallet}>Connect wallet</Button>
+		{:else}
+			<div class="space-y-2">
+				<form
+					method="POST"
+					action="?/linkAddress"
+					use:enhance={() => {
+						loading = true;
+						return async ({ result }) => {
+							searchLinkAddress();
+							await applyAction(result);
+						};
+					}}
+				>
+					<input name="address" value={$signerAddress} hidden />
+					<Button>New Link</Button>
+				</form>
 
-		</div>
+				<Button disabled={!$signerAddress} on:click={async () => defaultEvmStores.disconnect()}>
+					Disconnect
+				</Button>
+			</div>
 		{/if}
-		<p>{$signerAddress}</p>
-
 	</div>
-	
-</Background>
 
+	<div class="flex flex-col w-max bg-white p-6 rounded-lg shadow-md space-y-3 ml-5">
+		{#if $signerAddress}
+			<div class="space-y-2">
+				<p>{loading ? 'Searching' : message}</p>
+				<p>Address connected: {$signerAddress}</p>
+			</div>
+		{/if}
+	</div>
+</Background>
