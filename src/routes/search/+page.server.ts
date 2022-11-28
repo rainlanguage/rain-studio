fefd32648c835f06105fca3b52578ad52eaca137
NanezX <victor.nanezj@gmail.com>
2022-11-28 00:04:26 -0400

    removing unused files

--- src/routes/search/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { supabaseClient } from '$lib/supabaseClient';
-import { error } from '@sveltejs/kit';
-
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-	//
-}
