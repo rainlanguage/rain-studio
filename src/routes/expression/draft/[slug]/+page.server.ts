e9b1e38169cd3c8f2828b4c75049e0f002c569d4
Josh Hardy <jks.hardy@gmail.com>
2022-11-28 17:17:29 +0000

    removing logs and reintroducing error handling

--- src/routes/expression/draft/[slug]/+page.server.ts
+++ src/routes/expression/draft/[slug]/+page.server.ts
@@ -10,7 +10,7 @@ export async function load(event) {
     let userQuery, contractQuery, interpreterQuery
 
     const query = await supabaseClient.rpc('get_expression_by_slug', { slug }) as PostgrestSingleResponse<ExpressionRow[]>
-
+    console.log(query)
     if (query?.data?.[0]) {
         userQuery = await supabaseClient.from('profiles').select('*').filter('id', 'eq', query.data[0].user_id).single()
         if (query.data[0]?.contract) contractQuery = await supabaseClient.from('contracts').select('*, project (*)').filter('id', 'eq', query.data[0].contract).single()
@@ -18,12 +18,9 @@ export async function load(event) {
     }
 
     // catch all the errors
-    // if (query.error || userQuery?.error || contractQuery?.error || interpreterQuery?.error) throw error(404, 'Not found');
-    console.log(query.error || userQuery?.error || contractQuery?.error || interpreterQuery?.error)
+    if (query.error || userQuery?.error || contractQuery?.error || interpreterQuery?.error) throw error(404, 'Not found');
     // and make sure there's data for everything
-    console.log(!query.data.length || !userQuery?.data || !interpreterQuery?.data)
-    // if (!query.data.length || !userQuery?.data || !interpreterQuery?.data) throw error(404, 'Not found');
-    console.log({ expression: { ...query.data[0], user_id: userQuery.data, contract: contractQuery?.data as unknown as ContractRowFull, interpreter: interpreterQuery.data } })
+    if (!query.data.length || !userQuery?.data || !interpreterQuery?.data) throw error(404, 'Not found');
     return {
         expression: { ...query.data[0], user_id: userQuery.data, contract: contractQuery?.data as unknown as ContractRowFull, interpreter: interpreterQuery.data }
     }
--- src/routes/expression/draft/[slug]/edit/+page.svelte
+++ src/routes/expression/draft/[slug]/edit/+page.svelte
@@ -4,7 +4,6 @@
 	import type { ExpressionRowFull } from '$lib/types/types';
 
 	const expression: ExpressionRowFull = $page.data.expression;
-	$: console.log($page.data.expression);
 </script>
 
 <div class="flex-grow flex flex-col">
