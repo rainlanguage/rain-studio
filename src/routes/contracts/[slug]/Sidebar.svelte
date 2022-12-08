d8e3bc093a33ee06358535e2f8adb1f347b690a3
NanezX <victor.nanezj@gmail.com>
2022-12-07 22:02:36 -0400

    Social buttons on recent deployed expressions

--- src/routes/contracts/[slug]/Sidebar.svelte
+++ src/routes/contracts/[slug]/Sidebar.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-	import { Section, SectionBody, SectionHeading } from 'rain-svelte-components/package/section';
+	import { Heart, ArrowUp, ArrowDown, ChatBubbleLeft } from '@steeze-ui/heroicons';
+	import { Section, SectionBody } from 'rain-svelte-components/package/section';
+	import Formatter from 'rain-svelte-components/package/formatter/Formatter.svelte';
+	import { DisplayAddress, Ring, Button } from 'rain-svelte-components/package';
+
 	import SidebarHeading from '$lib/SidebarHeading.svelte';
 	import TimeAgo from '$lib/TimeAgo.svelte';
 	import UserAvatar from '$lib/UserAvatar.svelte';
-	import Formatter from 'rain-svelte-components/package/formatter/Formatter.svelte';
-	import { DisplayAddress, Ring } from 'rain-svelte-components/package';
-	import { Icon } from '@steeze-ui/svelte-icon';
-	import { User } from '@steeze-ui/heroicons';
+	import SocialButton from '$lib/SocialButton.svelte';
+
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { fly, fade } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 
 	let amountToShow = 5;
 	let showMore = false;
@@ -18,6 +20,12 @@
 	let _accountsData: { [key: string]: { username: string; avatar_url: string | null } };
 	let _loading = true;
 
+	////////////////////////////////////////
+	// TODO: Use data to social numbers like UpVotes/DownVotes, Likes, Comments
+	let _votes: { [key: number]: { upVote?: boolean; downVote?: boolean } } = {};
+	let _likes: { [key: number]: boolean } = {};
+	let _comments: { [key: number]: boolean } = {};
+
 	const seeAll = () => {
 		if (showMore) amountToShow = 5;
 		else amountToShow = _expressionSG.length;
@@ -25,6 +33,44 @@
 		showMore = !showMore;
 		expressionsToShow = _expressionSG?.slice(0, amountToShow);
 	};
+
+	const upVote = (id_: number) => {
+		if (_votes[id_] === undefined) {
+			_votes[id_] = {};
+		}
+
+		if (_votes[id_].upVote) {
+			_votes[id_].upVote = false;
+		} else {
+			_votes[id_].upVote = true;
+		}
+
+		_votes[id_].downVote = false;
+	};
+
+	const downVote = (id_: number) => {
+		if (_votes[id_] === undefined) {
+			_votes[id_] = {};
+		}
+
+		if (_votes[id_]?.downVote) {
+			_votes[id_].downVote = false;
+		} else {
+			_votes[id_].downVote = true;
+		}
+
+		_votes[id_].upVote = false;
+	};
+
+	const like = (id_: number) => {
+		_likes[id_] = !_likes[id_];
+	};
+
+	const comments = (id_: number) => {
+		_comments[id_] = !_comments[id_];
+	};
+
+	//////////////////////////////////////////
 
 	onMount(() => {
 		_expressionSG = $page.data.expressionSG;
@@ -41,8 +87,8 @@
 			{#if !_loading}
 				<div in:fade class="flex flex-col gap-y-4">
 					{#if expressionsToShow && expressionsToShow.length > 0}
-						{#each expressionsToShow as { name, config, event, account }}
-							<div class="border-b border-slate-200 pb-[10px] flex flex-col gap-y-2.5">
+						{#each expressionsToShow as { name, config, event, account }, index}
+							<div class="flex flex-col gap-y-2.5 border-b border-slate-200 pb-[10px]">
 								<div
 									class="font-mono text-[12px] leading-4 bg-neutral-100 tracking-[-0.01em] rounded-[5px] max-h-[89px] overflow-hidden gradient-mask-b-70"
 								>
@@ -62,6 +108,50 @@
 										{:else}
 											<DisplayAddress address={account.id} />
 										{/if}
+									</div>
+									<div class="flex">
+										<SocialButton
+											on:click={() => upVote(index)}
+											isActived={_votes[index]?.upVote}
+											colorActive="green"
+											icon={ArrowUp}
+											iconTheme="solid"
+										>
+											{43}
+										</SocialButton>
+										<SocialButton
+											on:click={() => downVote(index)}
+											isActived={_votes[index]?.downVote}
+											colorActive="red"
+											icon={ArrowDown}
+											iconTheme="solid"
+										>
+											{2}
+										</SocialButton>
+									</div>
+									<div>
+										<SocialButton
+											on:click={() => like(index)}
+											isActived={_likes[index]}
+											colorActive="red"
+											icon={Heart}
+											iconTheme={_likes[index] ? 'solid' : ''}
+											iconSize="13"
+										>
+											{43}
+										</SocialButton>
+									</div>
+									<div>
+										<SocialButton
+											on:click={() => comments(index)}
+											isActived={_comments[index]}
+											colorActive="green"
+											icon={ChatBubbleLeft}
+											iconTheme={_comments[index] ? 'solid' : ''}
+											iconSize="13"
+										>
+											{201}
+										</SocialButton>
 									</div>
 								</div>
 							</div>
