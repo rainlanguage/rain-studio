60e51eacc0c8ad59f0e677d9a75f0a16592a9a81
NanezX <victor.nanezj@gmail.com>
2022-12-19 15:44:29 -0400

    Fix when user update data

--- src/routes/settings/profile/+page.svelte
+++ src/routes/settings/profile/+page.svelte
@@ -11,33 +11,31 @@
 	let fullName: string | null = user?.full_name;
 	let website: string | null = user?.website;
 
-	let loading = false;
-	let isValid = false;
-
+	let updating = false;
 	let usernameInput: Input;
 
 	const usernameValidate = async (value: string) => {
+		if (user?.username.toLowerCase() == username.toLowerCase()) {
+			return;
+		}
+
 		const { data, error } = await supabaseClient
 			.from('wallet_users')
 			.select('*')
 			.eq('username', value.toLowerCase());
 
 		if (error) {
-			isValid = false;
 			return { error: 'Something went wrong' };
 		}
 
 		if (data && data.length) {
-			isValid = false;
 			return { error: "Username isn't available" };
 		}
-
-		isValid = true;
 	};
 
 	async function updateProfile() {
 		try {
-			loading = true;
+			updating = true;
 
 			await usernameInput.validate();
 			const _username = username.toLowerCase();
@@ -53,12 +51,18 @@
 			let { error } = await supabaseClient.from('wallet_users').upsert(updates);
 
 			if (error) throw error;
+
+			// Update the data in the component
+			user.username = updates.username;
+			user.avatar_url = updates.avatar_url;
+			user.full_name = updates.full_name;
+			user.website = updates.website;
 		} catch (error) {
 			if (error instanceof Error) {
 				alert(error.message);
 			}
 		} finally {
-			loading = false;
+			updating = false;
 		}
 	}
 </script>
@@ -80,8 +84,6 @@
 			<span slot="label">Website</span>
 		</Input>
 
-		<Button variant="black" classes="self-start" disabled={!isValid}
-			>{loading ? 'Loading...' : 'Update'}</Button
-		>
+		<Button variant="black" classes="self-start">{updating ? 'Updating...' : 'Update'}</Button>
 	</form>
 </div>
