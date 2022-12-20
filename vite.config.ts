5ccafd38ecf99cb2eeae9552a774135ab7986355
Josh Hardy <jks.hardy@gmail.com>
2022-12-20 12:43:08 +0000

    updating vite sentry plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -12,14 +12,20 @@ const config: UserConfig = {
 		project: "rain-studio",
 
 		// Specify the directory containing build artifacts
-		include: "./output",
+		include: "./svelte-kit",
 
 		// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
 		// and needs the `project:releases` and `org:read` scopes
 		authToken: process.env.SENTRY_AUTH_TOKEN,
 
 		// Optionally uncomment the line below to override automatic release name detection
-		// release: process.env.RELEASE,
+		// release: process.env.VERCEL_URL,
+
+		deploy: {
+			env: process.env.VERCEL_ENV || 'local',
+			name: process.env.VERCEL_URL || 'local',
+			url: process.env.VERCEL_URL || 'http://localhost:8080'
+		}
 	}),
 	],
 	optimizeDeps: {
