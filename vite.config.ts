7788b168443362338ef61076df28130f6d2d3301
Josh Hardy <jks.hardy@gmail.com>
2022-12-20 13:36:07 +0000

    removing vite sentry plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import sentryVitePlugin from "@sentry/vite-plugin";
+// import sentryVitePlugin from "@sentry/vite-plugin";
 
 const config: UserConfig = {
 	build: {
 		sourcemap: true, // Source map generation must be turned on
 	},
 	plugins: [sveltekit(),
-	sentryVitePlugin({
-		org: "rain-protocol",
-		project: "rain-studio",
+		// sentryVitePlugin({
+		// 	org: "rain-protocol",
+		// 	project: "rain-studio",
 
-		// Specify the directory containing build artifacts
-		include: "./svelte-kit",
+		// 	// Specify the directory containing build artifacts
+		// 	include: "./svelte-kit",
 
-		// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-		// and needs the `project:releases` and `org:read` scopes
-		authToken: process.env.SENTRY_AUTH_TOKEN,
+		// 	// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+		// 	// and needs the `project:releases` and `org:read` scopes
+		// 	authToken: process.env.SENTRY_AUTH_TOKEN,
 
-		// Optionally uncomment the line below to override automatic release name detection
-		// release: process.env.VERCEL_URL,
+		// 	// Optionally uncomment the line below to override automatic release name detection
+		// 	// release: process.env.VERCEL_URL,
 
-		// deploy: {
-		// 	env: process.env.VERCEL_ENV || 'local',
-		// 	// name: process.env.VERCEL_URL || 'local',
-		// 	url: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:8080'
-		// }
-	}),
+		// 	// deploy: {
+		// 	// 	env: process.env.VERCEL_ENV || 'local',
+		// 	// 	// name: process.env.VERCEL_URL || 'local',
+		// 	// 	url: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:8080'
+		// 	// }
+		// }),
 	],
 	optimizeDeps: {
 		exclude: ['javascript-time-ago']
