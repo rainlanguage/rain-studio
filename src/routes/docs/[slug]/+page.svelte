89801a548ab8bab6a5eeb6c408b8af459a246748
Josh Hardy <jks.hardy@gmail.com>
2023-01-09 19:55:00 +0000

    removing log

--- src/routes/docs/[slug]/+page.svelte
+++ src/routes/docs/[slug]/+page.svelte
@@ -4,7 +4,6 @@
 
 	type C = $$Generic<typeof SvelteComponentTyped<any, any, any>>;
 	$: component = $page.data.component as unknown as C;
-	$: console.log($page.data);
 </script>
 
 <h1 class="text-3xl">{$page.data.frontmatter.title}</h1>
