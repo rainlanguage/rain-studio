d0329e110ca988203f6b958b268642e423e85a83
NanezX <victor.nanezj@gmail.com>
2023-01-25 01:18:28 -0400

    using new context type

--- src/lib/types/context-types.ts
+++ src/lib/types/context-types.ts
@@ -1,8 +1,20 @@
-export type ContextInfo = {
+export type ContextInfo = UserContext | OrgContext;
+
+export type UserContext = ContextData & {
+	__ContextType: 'USER';
+};
+
+export type OrgContext = ContextData & {
+	orgId: string;
+	__ContextType: 'ORGANIZATION';
+};
+
+type ContextData = {
 	id: string;
 	name: string;
 	nickname: string;
 	avatar_url: string;
 	role: string;
 	website: string;
+	__ContextType: 'USER' | 'ORGANIZATION';
 };
--- src/routes/organizations/+page.server.ts
+++ src/routes/organizations/+page.server.ts
@@ -1,6 +1,7 @@
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
-import { redirect, fail } from '@sveltejs/kit';
+import { redirect, fail, error as sveltekitError } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { ContextInfo } from '$lib/types/context-types';
 
 export const load: PageServerLoad = async (event) => {
 	const { supabaseClient, session } = await getSupabase(event);
@@ -19,33 +20,93 @@ export const actions = {
 	newOrg: async (event) => {
 		const { request } = event;
 		const { supabaseClient, session } = await getSupabase(event);
+		if (session) {
+			const { user } = session;
+
+			const data = await request.formData();
+			const orgName = data.get('OrgName');
+			const orgNickname = data.get('OrgNickname');
+
+			const { data: dataOrg, error: errorOrg } = await supabaseClient
+				.from('organizations')
+				.insert({
+					name: orgName,
+					nickname: orgNickname.toLowerCase()
+				})
+				.select('*')
+				.single();
+
+			if (errorOrg) {
+				return fail(400, { error: errorOrg });
+			}
+
+			const { error: errorMember } = await supabaseClient.from('org_member').insert({
+				user_id: user.id,
+				org_id: dataOrg.id,
+				role: 'admin'
+			});
+
+			if (errorMember) {
+				return fail(400, { error: errorMember });
+			}
+		} else {
+			return fail(401, { error: 'User not logged.' });
+		}
+	},
+	changeContext: async (event) => {
+		const { request, cookies } = event;
+		const { supabaseClient, session } = await getSupabase(event);
+
+		if (!session) throw sveltekitError(401, { error: 'User not logged' });
+
 		const { user } = session;
-
 		const data = await request.formData();
-		const orgName = data.get('OrgName');
-		const orgNickname = data.get('OrgNickname');
+		const userId = data.get('user-id');
+		const memberId = data.get('member-id');
 
-		const { data: dataOrg, error: errorOrg } = await supabaseClient
-			.from('organizations')
-			.insert({
-				name: orgName,
-				nickname: orgNickname.toLowerCase()
-			})
-			.select('*')
-			.single();
+		let dataContext: ContextInfo;
 
-		if (errorOrg) {
-			return fail(400, { error: errorOrg });
+		if (userId) {
+			dataContext = {
+				id: user.id,
+				name: user.full_name ?? user.username,
+				nickname: user.username,
+				avatar_url: user.avatar_url,
+				role: 'user',
+				website: user.website,
+				__ContextType: 'USER'
+			};
 		}
 
-		const { error: errorMember } = await supabaseClient.from('org_member').insert({
-			user_id: user.id,
-			org_id: dataOrg.id,
-			role: 'admin'
+		if (memberId) {
+			const { error: errorOrg, data: dataOrg } = await supabaseClient
+				.from('org_member')
+				.select(`id, role, user_id, info_org: organizations(*)`)
+				.eq('id', memberId)
+				.single();
+
+			if (errorOrg) fail(400, { error: errorOrg });
+
+			if (dataOrg.user_id != user.id) fail(401, { error: 'Not have right to access.' });
+
+			dataContext = {
+				id: dataOrg.id,
+				orgId: dataOrg.info_org.id,
+				name: dataOrg.info_org.name,
+				nickname: dataOrg.info_org.nickname,
+				avatar_url: dataOrg.info_org.avatar_url,
+				role: dataOrg.role,
+				website: dataOrg.info_org.website,
+				__ContextType: 'ORGANIZATION'
+			};
+		}
+
+		// If the user is logged, should have the supabase cookie
+		const { expires_at } = JSON.parse(cookies.get('supabase-auth-token'));
+		cookies.set('rain-studio-context', JSON.stringify(dataContext), {
+			path: '/',
+			maxAge: expires_at,
+			httpOnly: false
 		});
-
-		if (errorMember) {
-			return fail(400, { error: errorMember });
-		}
 	}
 };
