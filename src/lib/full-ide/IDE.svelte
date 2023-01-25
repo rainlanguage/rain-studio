99314eaee2fc11e8873673e1ac231b8c34618190
Josh Hardy <jks.hardy@gmail.com>
2023-01-25 17:13:01 +0000

    ide mobile styles

--- src/lib/full-ide/IDE.svelte
+++ src/lib/full-ide/IDE.svelte
@@ -11,7 +11,17 @@
 		Pencil,
 		QuestionMarkCircle,
 		LockClosed,
-		LockOpen
+		LockOpen,
+		DocumentDuplicate,
+		Document,
+		CodeBracket,
+		Cog,
+		CircleStack,
+		TableCells,
+		DocumentText,
+		Beaker,
+		InformationCircle,
+		Lifebuoy
 	} from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import { Button, Modal, OpDocs, ParserInput } from 'rain-svelte-components/package';
@@ -30,9 +40,11 @@
 	import ModalChangeVisibilty from '$lib/expressions/ModalChangeVisibilty.svelte';
 	import type { Writable } from 'svelte/store';
 	import type { StateConfig } from 'rain-metadata/metadata-types/expression';
-	import { BigNumber } from 'ethers';
-	import { OverflowMenu } from 'rain-svelte-components/package/overflow-menu';
+	import { OverflowMenu, OverflowMenuItem } from 'rain-svelte-components/package/overflow-menu';
 	import HelpPanel from '$lib/HelpPanel.svelte';
+	import { breakpoint, Breakpoints } from '$lib/breakpoint-stores';
+	import { ShowPane } from '$lib/full-ide/types';
+	import MobileTabButton from '$lib/full-ide/MobileTabButton.svelte';
 
 	export let expression: ExpressionRowFull;
 	export let closeCallback: Function;
@@ -115,179 +127,224 @@
 	};
 
 	let saveACopy: boolean = false;
+
+	// code for showing different panes on mobile
+	let mobileActivePane = ShowPane.Expression;
+
+	$: isMobile = $breakpoint < Breakpoints.md;
 </script>
 
-<div class="flex justify-between ">
-	<div class="flex items-center">
-		<button on:click={back} class="flex items-center gap-x-1 p-3">
-			<div class="w-3">
-				<Icon src={ArrowUturnLeft} />
-			</div>
-			<span>Back</span>
-		</button>
-		<div class="h-full border-l border-gray-300" />
-		<!-- svelte-ignore a11y-click-events-have-key-events -->
-		{#if !asModal}
-			<div class="flex items-center gap-x-1 p-3">
-				{#if editingName}
-					<input
-						on:focusout={saveName}
-						bind:value={name}
-						bind:this={nameInput}
-						on:keyup={({ code }) => {
-							if (code == 'Enter') saveName();
-						}}
-					/>
-				{:else}
-					<span class="cursor-pointer" on:click={editName}>{name}</span>
-					<div on:click={editName} class="w-3 cursor-pointer">
-						<Icon theme="solid" src={Pencil} />
+<div class="flex flex-grow flex-col">
+	<div class="flex items-stretch justify-between">
+		<div class="flex items-stretch">
+			<div class="flex flex-col justify-center border-r border-gray-300">
+				<button on:click={back} class="flex items-center gap-x-1 p-3">
+					<div class="w-3">
+						<Icon src={ArrowUturnLeft} />
 					</div>
-				{/if}
+					<span class="hidden md:block">Back</span>
+				</button>
 			</div>
-			<div class="flex gap-x-1">
-				<span class="text-[14px] text-gray-600">
-					{expression.public ? 'Public' : 'Private'}
-				</span>
-				<div class="mr-3 w-3.5 pb-0.5">
-					<Icon src={expression.public ? LockOpen : LockClosed} />
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			{#if !asModal}
+				<div class="flex items-center gap-x-1 p-3">
+					{#if editingName}
+						<input
+							on:focusout={saveName}
+							bind:value={name}
+							bind:this={nameInput}
+							on:keyup={({ code }) => {
+								if (code == 'Enter') saveName();
+							}}
+							class="text-sm md:text-base"
+						/>
+					{:else}
+						<span class="cursor-pointer text-sm md:text-base" on:click={editName}>{name}</span>
+						<div on:click={editName} class="w-3 cursor-pointer">
+							<Icon theme="solid" src={Pencil} />
+						</div>
+					{/if}
 				</div>
-			</div>
-		{/if}
-		{#if saving}
-			<div
-				transition:fade={{ duration: 100 }}
-				class="flex items-center gap-x-1 rounded-xl border border-gray-200 bg-gray-100 p-1"
-			>
-				<div class="w-4 animate-spin text-gray-500">
-					<Icon src={ArrowPath} />
+				<div class="flex items-center gap-x-1">
+					<span class="hidden text-[14px] text-gray-600 md:block">
+						{expression.public ? 'Public' : 'Private'}
+					</span>
+					<div class="mr-3 w-3.5 pb-0.5">
+						<Icon src={expression.public ? LockOpen : LockClosed} />
+					</div>
 				</div>
-				<span class="text-xs text-gray-500">Saving</span>
-			</div>
-		{/if}
+			{/if}
+			{#if saving}
+				<div
+					class="fixed left-0 right-0 bottom-3 flex flex-row justify-center self-center md:relative md:bottom-auto md:left-auto md:right-auto md:flex-col md:items-center"
+				>
+					<div
+						transition:fade={{ duration: 100 }}
+						class="mx-auto flex items-center gap-x-1 rounded-xl border border-gray-200 bg-gray-100 p-1"
+					>
+						<div class="w-4 animate-spin text-gray-500">
+							<Icon src={ArrowPath} />
+						</div>
+						<span class="text-xs text-gray-500">Saving</span>
+					</div>
+				</div>
+			{/if}
+		</div>
+		<div class="flex gap-x-2 py-3 px-3">
+			{#if !asModal}
+				<OverflowMenu position="right">
+					<OverflowMenuItem icon={Eye} href={`/expression/draft/${expression.sharable_slug}`}>
+						View
+					</OverflowMenuItem>
+					<OverflowMenuItem
+						icon={PaperAirplane}
+						on:click={() => {
+							copyAndEmit(
+								`${$page.url.origin}/expression/draft/${expression.sharable_slug}`,
+								'Link copied to clipboard'
+							);
+						}}
+					>
+						Share
+					</OverflowMenuItem>
+					<OverflowMenuItem
+						on:click={() => {
+							saveACopy = true;
+						}}
+						icon={DocumentDuplicate}
+					>
+						Save a copy
+					</OverflowMenuItem>
+					<OverflowMenuItem
+						icon={expression.public ? LockClosed : LockOpen}
+						on:click={() => (changeVisiblityModal = true)}
+					>
+						Make {expression.public ? 'private' : 'public'}
+					</OverflowMenuItem>
+				</OverflowMenu>
+			{/if}
+			<OverflowMenu position="right" autoWidth>
+				<svelte:fragment slot="button">
+					<Button variant="transparent" icon={QuestionMarkCircle} size="small">Help</Button>
+				</svelte:fragment>
+				<div class="p-4">
+					<HelpPanel />
+				</div>
+			</OverflowMenu>
+		</div>
 	</div>
-	<div class="flex gap-x-2 py-3 px-3">
-		{#if !asModal}
-			<Button
-				on:click={() => {
-					saveACopy = true;
-				}}
-				variant="transparent"
-				size="small">Save a copy</Button
+
+	<div class="flex flex-grow items-stretch">
+		<div class="relative flex-grow overflow-hidden bg-red-100">
+			<Splitpanes
+				theme="modern-theme"
+				style="position: absolute; top: 0; bottom: 0; left:0; right:0;"
 			>
-			<Button
-				on:click={() => goto(`/expression/draft/${expression.sharable_slug}`)}
-				size="small"
-				variant="transparent"
-				icon={Eye}>View</Button
-			>
-			<Button
-				on:click={() => {
-					copyAndEmit(
-						`${$page.url.origin}/expression/draft/${expression.sharable_slug}`,
-						'Link copied to clipboard'
-					);
-				}}
-				variant="transparent"
-				icon={PaperAirplane}
-				size="small">Share</Button
-			>
-			<Button
-				on:click={() => (changeVisiblityModal = true)}
-				variant="transparent"
-				icon={expression.public ? LockClosed : LockOpen}
-				size="small">Make {expression.public ? 'private' : 'public'}</Button
-			>
-		{/if}
-		<OverflowMenu position="right" autoWidth>
-			<svelte:fragment slot="button">
-				<Button variant="transparent" icon={QuestionMarkCircle} size="small">Help</Button>
-			</svelte:fragment>
-			<div class="p-4">
-				<HelpPanel />
-			</div>
-		</OverflowMenu>
+				{#if !(isMobile && !(mobileActivePane == ShowPane.Environment))}
+					<Pane size={isMobile ? 100 : 25} minSize={15}>
+						<div class="flex flex-col border-t border-gray-300 bg-white">
+							<div class="flex flex-col gap-y-2 border-b border-gray-300 py-4 px-2">
+								<span class="text-[10px] uppercase text-gray-600">Writing for</span>
+								<ExpressionEnv {expression} />
+							</div>
+						</div>
+					</Pane>
+				{/if}
+				{#if !(isMobile && !(mobileActivePane == ShowPane.Expression || mobileActivePane == ShowPane.MockData || mobileActivePane == ShowPane.Notes))}
+					<Pane size={isMobile ? 100 : undefined} minSize={10}>
+						<Splitpanes horizontal firstSplitter theme="modern-theme">
+							{#if !(isMobile && !(mobileActivePane == ShowPane.Expression))}
+								<Pane size={isMobile ? 100 : 50} minSize={10}>
+									<div class="flex h-full flex-col">
+										<PanelHeader>Expression</PanelHeader>
+										<div class="flex min-h-0 flex-grow flex-col overflow-scroll p-2">
+											<ParserInput bind:raw={raw_expression} bind:vmStateConfig />
+										</div>
+									</div>
+								</Pane>
+							{/if}
+							{#if !(isMobile && !(mobileActivePane == ShowPane.MockData))}
+								<Pane size={isMobile ? 100 : 30} minSize={10}>
+									<div class="h-full">
+										<PanelHeader>Mock data</PanelHeader>
+										<div class="h-full overflow-scroll p-2">
+											{#if contextColumns}
+												<div class="pt-4 pb-2 text-xs uppercase text-gray-500">Context</div>
+												<ContextGrid {contextColumns} bind:mockContext />
+											{/if}
+											{#if hasSignedContext}
+												<div class="pt-4 pb-2 text-xs uppercase text-gray-500">
+													{expression.contract ? 'Signed context' : 'Context'}
+												</div>
+												<SignedContext bind:signedContext />
+											{/if}
+										</div>
+									</div>
+								</Pane>
+							{/if}
+							{#if !asModal}
+								{#if !(isMobile && !(mobileActivePane == ShowPane.Notes))}
+									<Pane size={isMobile ? 100 : 20} minSize={10}>
+										<div class="flex h-full flex-grow flex-col">
+											<PanelHeader>Notes</PanelHeader>
+											<div class="flex flex-grow flex-col gap-y-2 p-2">
+												<Tags bind:tags onlyUnique labelShow allowBlur labelText="Tags" />
+												<textarea
+													class="flex-grow self-stretch justify-self-stretch whitespace-pre-wrap outline-0"
+													bind:value={notes}
+												/>
+											</div>
+										</div>
+									</Pane>
+								{/if}
+							{/if}
+						</Splitpanes>
+					</Pane>
+				{/if}
+				{#if !(isMobile && !(mobileActivePane == ShowPane.Simulator || mobileActivePane == ShowPane.Docs))}
+					<Pane size={isMobile ? 100 : 25} minSize={15}>
+						<Splitpanes horizontal firstSplitter theme="modern-theme">
+							{#if !(isMobile && !(mobileActivePane == ShowPane.Simulator))}
+								<Pane size={isMobile ? 100 : 50} minSize={10}>
+									<div class="flex h-full flex-grow flex-col">
+										<PanelHeader>Simulator</PanelHeader>
+										<div class="relative flex-grow overflow-y-scroll p-2">
+											<SimulatedOutput {vmStateConfig} {context} />
+										</div>
+									</div>
+								</Pane>
+							{/if}
+							{#if !(isMobile && !(mobileActivePane == ShowPane.Docs))}
+								<Pane size={isMobile ? 100 : 50} minSize={10}>
+									<div class="flex h-full flex-grow flex-col">
+										<div class="flex-shrink">
+											<PanelHeader>Available words</PanelHeader>
+										</div>
+										<div class="relative flex-grow">
+											<div class="absolute inset-0">
+												<OpDocs />
+											</div>
+										</div>
+									</div>
+								</Pane>
+							{/if}
+						</Splitpanes>
+					</Pane>
+				{/if}
+			</Splitpanes>
+		</div>
+		<div
+			class="borderg-gray-300 flex flex-col justify-items-start border-l border-t bg-gray-100 p-1 md:hidden"
+		>
+			<MobileTabButton icon={CodeBracket} pane={ShowPane.Expression} bind:mobileActivePane />
+			<MobileTabButton icon={InformationCircle} pane={ShowPane.Environment} bind:mobileActivePane />
+			<MobileTabButton icon={Beaker} pane={ShowPane.Simulator} bind:mobileActivePane />
+			<MobileTabButton icon={TableCells} pane={ShowPane.MockData} bind:mobileActivePane />
+			<MobileTabButton icon={DocumentText} pane={ShowPane.Notes} bind:mobileActivePane />
+			<MobileTabButton icon={Lifebuoy} pane={ShowPane.Docs} bind:mobileActivePane />
+		</div>
 	</div>
 </div>
-
-<Splitpanes theme="modern-theme" style="flex-grow: 1; height:1px;">
-	<Pane size={25} minSize={15}>
-		<div class="flex flex-col border-t border-gray-300 bg-white">
-			<div class="flex flex-col gap-y-2 border-b border-gray-300 py-4 px-2">
-				<span class="text-[10px] uppercase text-gray-600">Writing for</span>
-				<ExpressionEnv {expression} />
-			</div>
-		</div>
-	</Pane>
-	<Pane minSize={10}>
-		<Splitpanes horizontal firstSplitter theme="modern-theme">
-			<Pane size={50} minSize={10}>
-				<div class="flex h-full flex-col">
-					<PanelHeader>Expression</PanelHeader>
-					<div class="flex min-h-0 flex-grow flex-col overflow-scroll p-2">
-						<ParserInput bind:raw={raw_expression} bind:vmStateConfig />
-					</div>
-				</div>
-			</Pane>
-			<Pane size={30} minSize={10}>
-				<div class="h-full">
-					<PanelHeader>Mock data</PanelHeader>
-					<div class="h-full overflow-scroll p-2">
-						{#if contextColumns}
-							<div class="pt-4 pb-2 text-xs uppercase text-gray-500">Context</div>
-							<ContextGrid {contextColumns} bind:mockContext />
-						{/if}
-						{#if hasSignedContext}
-							<div class="pt-4 pb-2 text-xs uppercase text-gray-500">
-								{expression.contract ? 'Signed context' : 'Context'}
-							</div>
-							<SignedContext bind:signedContext />
-						{/if}
-					</div>
-				</div>
-			</Pane>
-			{#if !asModal}
-				<Pane size={20} minSize={10}>
-					<div class="flex h-full flex-grow flex-col">
-						<PanelHeader>Notes</PanelHeader>
-						<div class="flex flex-grow flex-col gap-y-2 p-2">
-							<Tags bind:tags onlyUnique labelShow allowBlur labelText="Tags" />
-							<textarea
-								class="flex-grow self-stretch justify-self-stretch whitespace-pre-wrap outline-0"
-								bind:value={notes}
-							/>
-						</div>
-					</div>
-				</Pane>
-			{/if}
-		</Splitpanes>
-	</Pane>
-	<Pane size={25} minSize={15}>
-		<Splitpanes horizontal firstSplitter theme="modern-theme">
-			<Pane size={50} minSize={10}>
-				<div class="flex h-full flex-grow flex-col">
-					<PanelHeader>Simulator</PanelHeader>
-					<div class="relative flex-grow overflow-y-scroll p-2">
-						<SimulatedOutput {vmStateConfig} {context} />
-					</div>
-				</div>
-			</Pane>
-			<Pane size={50} minSize={10}>
-				<div class="flex h-full flex-grow flex-col">
-					<div class="flex-shrink">
-						<PanelHeader>Available words</PanelHeader>
-					</div>
-					<div class="relative flex-grow">
-						<div class="absolute inset-0">
-							<OpDocs />
-						</div>
-					</div>
-				</div>
-			</Pane>
-		</Splitpanes>
-	</Pane>
-</Splitpanes>
-
 <Modal open={saveACopy}>
 	<ForkExpression
 		expression={{
