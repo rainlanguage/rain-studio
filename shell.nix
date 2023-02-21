b75586acf1f87f55084599717866be87b85d8e29
marcusnewton <marcus.newton@hey.com>
2023-02-21 16:57:53 +1100

    add nix shell config

---
+++ shell.nix
@@ -0,0 +1,29 @@
+let
+  pkgs = import
+    (builtins.fetchTarball {
+      name = "nixos-unstable-2023-02-21";
+      url = "https://github.com/nixos/nixpkgs/archive/b69883faca9542d135fa6bab7928ff1b233c167f.tar.gz";
+      sha256 = "sha256:1r846d1fb7qgqrnnkjss9r4idm9gnmh9qfvj751gfkjz63mrn4k5";
+    })
+    { };
+
+  dev = pkgs.writeShellScriptBin "dev" ''
+    yarn dev
+  '';
+in
+pkgs.stdenv.mkDerivation {
+  name = "shell";
+  buildInputs = [
+    pkgs.nixpkgs-fmt
+    pkgs.nodejs-16_x
+    pkgs.typescript
+    pkgs.yarn
+    dev
+  ];
+
+  shellHook = ''
+    export PATH=$( yarn bin ):$PATH
+    # keep it fresh
+    yarn
+  '';
+}
