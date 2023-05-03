5f1e75aa76372e939411900c54fb35806a1cb205
NanezX <victor.nanezj@gmail.com>
2023-05-03 17:29:14 -0400

    updating comment

--- src/routes/contracts/+page.server.ts
+++ src/routes/contracts/+page.server.ts
@@ -3,7 +3,7 @@ import { error as errorKit } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	// Using this function to retrieve 50 (limit) contract starting with the 0 offset
+	// Using this function to retrieve 52 (limit) contract starting with the 0 offset
 	const { data, error } = await supabaseClient.rpc('get_contracts_address_no_filters', {
 		offset_: 0
 	});
