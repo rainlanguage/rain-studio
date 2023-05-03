a86a6fe4d618fefe6d4d0e544fb34f2951c9940c
NanezX <victor.nanezj@gmail.com>
2023-05-03 18:50:19 -0400

    use rpc fnc to get int contracts with pagination

--- src/routes/contracts/+page.server.ts
+++ src/routes/contracts/+page.server.ts
@@ -3,11 +3,9 @@ import { error as errorKit } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const { data, error } = await supabaseClient.rpc('get_contracts_with_addresses_by_filters', {
-		//  Empty search value will not filter nothing by this value
-		search_value: '',
-		// The `-1` indicate to not filter by networks
-		selected_networks: [-1]
+	// Using this function to retrieve 52 (limit) contract starting with the 0 offset
+	const { data, error } = await supabaseClient.rpc('get_contracts_address_no_filters', {
+		offset_: 0
 	});
 
 	if (error) throw errorKit(404, 'Not found');
