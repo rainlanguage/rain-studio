92d184ac5144090c52a50fac9397eed95c961745
NanezX <victor.nanezj@gmail.com>
2023-05-03 14:07:40 -0400

    add change on contracts query

--- src/routes/contracts/+page.server.ts
+++ src/routes/contracts/+page.server.ts
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const query = await supabaseClient.from('contracts_new').select('*, contract_addresses_new(id, address, chainId)');
+	const query = await supabaseClient.from('contracts_new').select('*, contract_addresses_new(id, address, chain_id)');
 
 	if (query.error) throw error(404, 'Not found');
 
