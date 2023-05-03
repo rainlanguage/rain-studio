8b2ac26ea5ab7e7d33e73257760663bf6ba443f0
NanezX <victor.nanezj@gmail.com>
2023-05-03 17:24:39 -0400

    Use function with pagination

--- src/routes/contracts/+page.server.ts
+++ src/routes/contracts/+page.server.ts
@@ -3,26 +3,12 @@ import { error as errorKit } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const { data, error } = await supabaseClient.rpc('get_contracts_with_addresses_by_filters', {
-		//  Empty search value will not filter nothing by this value
-		search_value: '',
-		// The `-1` indicate to not filter by networks
-		selected_networks: [-1]
+	// Using this function to retrieve 50 (limit) contract starting with the 0 offset
+	const { data, error } = await supabaseClient.rpc('get_contracts_address_no_filters', {
+		offset_: 0
 	});
 
 	if (error) throw errorKit(404, 'Not found');
 
 	return { contract: data };
 }
-
-// import { supabaseClient } from '$lib/supabaseClient';
-// import { error } from '@sveltejs/kit';
-
-// /** @type {import('./$types').PageServerLoad} */
-// export async function load() {
-// 	const query = await supabaseClient.from('contracts_new').select('*, contract_addresses_new(id, address, chain_id)');
-
-// 	if (query.error) throw error(404, 'Not found');
-
-// 	return { contract: query.data };
-// }
