cf9ba4b1846d2c8f1ba6a55f6023fc710dff906d
NanezX <victor.nanezj@gmail.com>
2023-05-08 00:44:02 -0400

    pagination button for contracts page

--- src/routes/contracts/+page.svelte
+++ src/routes/contracts/+page.svelte
@@ -2,9 +2,16 @@
 	import { page } from '$app/stores';
 	import ContractCard from '$lib/contracts/ContractCard.svelte';
 	import Background from '$lib/Background.svelte';
-	import { FilterSet, FilterGroup, Input, Ring } from '@rainprotocol/rain-svelte-components';
+	import {
+		FilterSet,
+		FilterGroup,
+		Input,
+		Ring,
+		Button
+	} from '@rainprotocol/rain-svelte-components';
 	import { debounce } from 'lodash-es';
 	import { networkOptions } from '$lib/utils';
+	import { ChevronLeft, ChevronRight } from '@steeze-ui/heroicons';
 
 	$: contracts = $page.data.contract;
 	$: counterContracts = $page.data.counterContracts;
@@ -15,6 +22,8 @@
 
 	let searchValue: string;
 	let loading_: boolean = false;
+	let offset_: number = 0;
+	let indexSelected_: number = 0;
 
 	const getContractsFiltered = async (searchValue_: string, selectedNetworks_: Array<number>) => {
 		const resp = await fetch(`${$page.url.origin}/contracts`, {
@@ -22,13 +31,9 @@
 			body: JSON.stringify({
 				searchValue: searchValue_,
 				selectedNetworks: selectedNetworks_,
-				// TODO: Add tracking offset for the pagination FE
-				offset_: 0
+				offset_
 			})
 		});
-
-		console.log(searchValue_);
-		console.log(selectedNetworks_);
 
 		if (resp.ok) {
 			const { contractsFiltered, counterFiltered } = await resp.json();
@@ -47,6 +52,38 @@
 		getContractsFilteredDebounced(searchValue_, selectedNetworks_);
 	};
 
+	const changePagination = (num_: number, single_?: 'prev' | 'next') => {
+		const _minOffset = 0;
+		const _maxOffset = (contractsL - 1) * 52;
+
+		if (!single_) {
+			offset_ = num_ * 52;
+			indexSelected_ = num_;
+		}
+
+		if (single_ == 'prev') {
+			const aux = offset_ - 52;
+
+			if (aux < _minOffset) {
+				offset_ = _minOffset;
+			} else {
+				offset_ = aux;
+				indexSelected_ -= 1;
+			}
+		}
+
+		if (single_ == 'next') {
+			const aux = offset_ + 52;
+
+			if (aux > _maxOffset) {
+				offset_ = _maxOffset;
+			} else {
+				offset_ = aux;
+				indexSelected_ += 1;
+			}
+		}
+	};
+
 	$: networkFilterToShow = () => {
 		// If no network selected (component initialization) OR length is 1 and is -1 (which means)
 		// `All` networks is selected because that option is exclusive
@@ -58,7 +95,7 @@
 			.join(', ');
 	};
 
-	$: searchValue, selectedNetworks, callFilterWithDebounce(searchValue, selectedNetworks);
+	$: searchValue, selectedNetworks, offset_, callFilterWithDebounce(searchValue, selectedNetworks);
 </script>
 
 <div class="m-2 flex w-3/4 flex-row gap-x-10 self-center">
@@ -93,10 +130,9 @@
 </div>
 
 <Background alignItems="items-start">
-	<div class="container mx-auto flex py-8 px-4 sm:px-0">
-		<!-- <div class="w-1/3" /> -->
+	<div class="container mx-auto flex py-8 px-4 sm:px-0 ">
 		{#if contracts && contracts.length}
-			<div class="justify-self-center">
+			<div class="flex flex-col gap-y-8 justify-self-center">
 				<div class="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
 					{#each contracts as contract}
 						<a href={`/contracts/${contract.slug}`}>
@@ -104,9 +140,35 @@
 						</a>
 					{/each}
 				</div>
-				<div class="flex justify-center gap-x-2">
-					Next page elements to create:
-					<p>{contractsL}</p>
+				<div class="flex justify-center">
+					<Button
+						classes="text-neutral-600 border border-neutral-200 hover:bg-neutral-200"
+						dual={'left'}
+						size="small"
+						icon={ChevronLeft}
+						on:click={() => changePagination(0, 'prev')}
+					/>
+
+					{#each Array(contractsL) as _, index}
+						<button
+							class={`disabled flex items-center border border-neutral-200 px-2 text-neutral-600 ${
+								index == indexSelected_ ? '' : 'hover:bg-neutral-200'
+							}`}
+							on:click={() => changePagination(index)}
+							disabled={index == indexSelected_}
+						>
+							{index + 1}
+						</button>
+					{/each}
+
+					<Button
+						size="small"
+						classes="text-neutral-600 border border-neutral-200 hover:bg-neutral-200"
+						dual={'right'}
+						icon={ChevronRight}
+						iconPosition={'right'}
+						on:click={() => changePagination(0, 'next')}
+					/>
 				</div>
 			</div>
 		{:else}
