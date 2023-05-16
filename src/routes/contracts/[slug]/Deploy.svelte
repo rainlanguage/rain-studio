f059b3bbb6207cefd153d11c6dc0e16c4ce8721e
NanezX <victor.nanezj@gmail.com>
2023-05-16 18:15:29 -0400

    wip cross deployment

--- src/routes/contracts/[slug]/Deploy.svelte
+++ src/routes/contracts/[slug]/Deploy.svelte
@@ -1,7 +1,20 @@
-<script lang='ts'>
- 
-import { getContractDeployTxData, RainContracts, RainNetworks } from "rain-x-deploy"
-RainContracts.OrderBook
+<script lang="ts">
+	import ConnectWallet from '$lib/connect-wallet/ConnectWallet.svelte';
+	import { Button } from '@rainprotocol/rain-svelte-components';
+	import { getContractDeployTxData, RainNetworks, type DISpair } from 'rain-x-deploy';
+	import { provider, signer } from 'svelte-ethers-store';
 
+	const pave = async () => {
+		const addr_ = await $signer.getAddress();
+		console.log('addr: ', addr_);
+	};
 </script>
-Add deploy
+
+{#if !$signer}
+	<div class="flex flex-col gap-y-6 self-center">
+		<p>You should connect your wallet before interacting with the newtork</p>
+		<ConnectWallet />
+	</div>
+{:else}
+	<Button on:click={pave}>Click here</Button>
+{/if}
