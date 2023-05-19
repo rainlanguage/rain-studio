2f907447ae83ba823f31b5bdc67ede1b97f67abf
SocioDroid <sociodroid2019@gmail.com>
2023-05-20 01:19:50 +0530

    Initialized tests

---
+++ tests/Pages/Homepage.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, type Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/');
+});
+
+test.describe('Studio Homepage test', () => {
+  test('has title', async ({ page }) => {
+    await expect(page).toHaveTitle(/Rain Studio/);
+  });
+
+  test('sign up button', async ({ page }) => {
+    await page.getByRole('button', { name: 'Sign up' }).click();
+    await expect(page).toHaveURL(/sign-in/);
+  });
+
+  test('expressions link', async ({ page }) => {
+    await page.getByRole('button', { name: 'Discover' }).first().click();
+    await expect(page).toHaveURL(/expressions/);
+    
+    await page.goto('/');
+    await page.getByRole('button', { name: 'Discover community expressions' }).click();
+    await expect(page).toHaveURL(/expressions/);
+  });
+
+  test('contracts link', async ({ page }) => {
+    await page.getByRole('link', { name: 'Contracts' }).first().click();
+    await expect(page).toHaveURL(/contracts/);
+  });
+ 
+  test('docs link', async ({ page }) => {
+    await page.getByRole('link', { name: 'Docs' }).first().click();
+    await expect(page).toHaveURL(/docs/);
+
+    await page.goto('/');
+    await page.getByRole('button', { name: 'Learn how to write expressions' }).click();
+    await expect(page).toHaveURL(/introduction-to-rain-studio/);
+  });
+
+});
