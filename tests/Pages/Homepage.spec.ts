da0bfc7dc54df2512a66716ab83ebd7f2ce91282
SocioDroid <sociodroid2019@gmail.com>
2023-05-23 20:49:04 +0530

    CI setup

--- tests/Pages/Homepage.spec.ts
+++ tests/Pages/Homepage.spec.ts
@@ -5,6 +5,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('Studio Homepage test', () => {
+
   test('has title', async ({ page }) => {
     await expect(page).toHaveTitle(/Rain Studio/);
   });
@@ -17,7 +18,9 @@ test.describe('Studio Homepage test', () => {
   test('expressions link', async ({ page }) => {
     await page.getByRole('button', { name: 'Discover' }).first().click();
     await expect(page).toHaveURL(/expressions/);
-    
+  });
+
+  test('community expressions link', async ({ page }) => {
     await page.goto('/');
     await page.getByRole('button', { name: 'Discover community expressions' }).click();
     await expect(page).toHaveURL(/expressions/);
@@ -27,11 +30,14 @@ test.describe('Studio Homepage test', () => {
     await page.getByRole('link', { name: 'Contracts' }).first().click();
     await expect(page).toHaveURL(/contracts/);
   });
- 
+
   test('docs link', async ({ page }) => {
     await page.getByRole('link', { name: 'Docs' }).first().click();
     await expect(page).toHaveURL(/docs/);
 
+  });
+
+  test('intro docs link', async ({ page }) => {
     await page.goto('/');
     await page.getByRole('button', { name: 'Learn how to write expressions' }).click();
     await expect(page).toHaveURL(/introduction-to-rain-studio/);
--- tests/utils/fixtures.ts
+++ tests/utils/fixtures.ts
@@ -14,7 +14,7 @@ export const test = baseTest.extend<{
         wallet: "metamask",
         version: MetaMaskWallet.recommendedVersion,
         seed: "test test test test test test test test test test test junk", // Hardhat's default https://hardhat.org/hardhat-network/docs/reference#accounts
-        headless: false,
+        headless: true,
       });
 
       // Add Hardhat as a custom network
