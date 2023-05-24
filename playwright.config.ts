1b9ed043ca1e99cfdaaedcb3878fcdc27aded15c
SocioDroid <sociodroid2019@gmail.com>
2023-05-24 23:22:30 +0530

    Hardcoded hash_key

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,17 +25,17 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:4173',
-    baseURL: 'https://studio.rainprotocol.xyz',
+    baseURL: 'http://127.0.0.1:4173',
+    // baseURL: 'https://studio.rainprotocol.xyz',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  // webServer: {
-  //   command: 'npm run build && npm run preview',
-  //   url: 'http://127.0.0.1:4173',
-  //   reuseExistingServer: true
-  // },
+  webServer: {
+    command: 'npm run build && npm run preview',
+    url: 'http://127.0.0.1:4173',
+    reuseExistingServer: true
+  },
   /* Configure projects for major browsers */
   projects: [
     {
