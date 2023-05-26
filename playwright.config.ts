25ce0834772282192fbdc2b0c8bc79718eb377d0
SocioDroid <sociodroid2019@gmail.com>
2023-05-27 01:42:27 +0530

    Switching to headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    
+    headless: true,
     baseURL: 'http://localhost:4173',
     // baseURL: 'https://studio.rainprotocol.xyz',
 
