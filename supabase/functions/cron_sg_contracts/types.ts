10c445d259b9bd778bca64f7181e5f88dd3f9177
NanezX <victor.nanezj@gmail.com>
2023-07-04 16:02:15 -0400

    cron for only contract and contract addresses

---
+++ supabase/functions/cron_sg_contracts/types.ts
@@ -0,0 +1,244 @@
+// deno-lint-ignore-file no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Database } from '../../../src/lib/types/generated-db-types.ts';
+import type { Client } from 'https://esm.sh/v111/@urql/core@3.2.0';
+import type { SupabaseClient } from 'https://esm.sh/@supabase/supabase-js@2.0.0-rc.12';
+
+export { Client as SubgraphClient, SupabaseClient };
+
+export type ContractDB = Database['public']['Tables']['contracts_new']['Row'] & {
+	contract_addresses_new: Array<Database['public']['Tables']['contract_addresses_new']['Row']>;
+};
+
+export type DeployerDB = Database['public']['Tables']['deployers']['Row'] & {
+	deployers_addresses: Array<Database['public']['Tables']['deployers_addresses']['Row']>;
+};
+
+export type RainterpreterDB = Database['public']['Tables']['rainterpreters']['Row'] & {
+	rainterpreter_addresses: Array<Database['public']['Tables']['rainterpreter_addresses']['Row']>;
+};
+
+export type Rainterpreter_storesDB = Database['public']['Tables']['rainterpreter_stores']['Row'] & {
+	rainterpreter_store_addresses: Array<
+		Database['public']['Tables']['rainterpreter_store_addresses']['Row']
+	>;
+};
+
+/**
+ * @type
+ * The contract format that comes from the SG query
+ */
+export type ContractSG = {
+	/**
+	 * ID of the entity from the subgraph
+	 */
+	id: string;
+	/**
+	 * Hash of the bytcode of the this contract
+	 */
+	bytecodeHash: string;
+	/**
+	 * MetaV1 document decoded
+	 */
+	meta: MetaDocumentSG;
+	/**
+	 * Type of the contract. It could be 'proxy' or 'contract'
+	 */
+	type: string;
+	/**
+	 * Optional: Implementation information
+	 */
+	implementation: {
+		id: string;
+	} | null;
+};
+
+/**
+ * @type
+ * The meta document format that comes from the SG query
+ */
+export type MetaDocumentSG = {
+	/**
+	 * Raw meta bytes emited by the contract
+	 */
+	metaBytes: string;
+	/**
+	 * Array of the meta content decoded from the Meta emited by the contract
+	 */
+	content: Array<MetaContentV1SG>;
+};
+
+/**
+ * @type
+ * The meta content V1 format that comes from the SG query
+ */
+export type MetaContentV1SG = {
+	/**
+	 * Payload bytes of this part of the RainDocument
+	 */
+	payload: string;
+	/**
+	 * MagicNumber to identify and filter each enty of the RainDocument
+	 */
+	magicNumber: string;
+	/**
+	 * The content type of the payload
+	 */
+	contentType: string;
+	/**
+	 * The content encoding that is using the payload
+	 */
+	contentEncoding: string;
+	/**
+	 * The content language that is using the payload
+	 */
+	contentLanguage: string;
+};
+
+/**
+ * @type
+ * The expression deployer format that comes from the SG query
+ */
+export type ExpressionDeployerSG = {
+	/**
+	 * ID of the entity from the subgraph
+	 */
+	id: string;
+	/**
+	 * Hash of the bytcode of the this contract
+	 */
+	bytecodeHash: string;
+	/**
+	 * MetaV1 document decoded
+	 */
+	meta: MetaDocumentSG;
+};
+
+/**
+ * @type
+ * The rainterpreter format that comes from the SG query
+ */
+export type InterpreterSG = {
+	/**
+	 * The ID is the bytecode hash of all the Interpreters Instances
+	 */
+	id: string;
+	/**
+	 * The instances are each interpreter address that have the same bytecode
+	 */
+	instances: Array<{
+		/**
+		 * Address of the instance
+		 */
+		id: string;
+	}>;
+};
+
+/**
+ * @type
+ * The rainterpreterStore format that comes from the SG query
+ */
+export type RainterpreterStoreSG = {
+	/**
+	 * The ID is the bytecode hash of all the RainterpreterStore Instances
+	 */
+	id: string;
+	/**
+	 * The instances are each store address that have the same bytecode
+	 */
+	instances: Array<{
+		/**
+		 * Address of the instance
+		 */
+		id: string;
+	}>;
+};
+
+export type ABI = any[];
+
+/**
+ * Contract Meta JSON from the Meta
+ */
+export type ContractMeta = {
+	name: string;
+	source: string;
+	desc: string;
+	alias: string;
+	methods: Array<{
+		name: string;
+		desc: string;
+		inputs: any[];
+		expressions: any[];
+	}>;
+};
+
+/**
+ * Metadata format to be stored in the Database
+ */
+export type Metadata = {
+	name: string;
+	source: string;
+	description: string;
+	version: {
+		major: number;
+		minor: number;
+	};
+	inputs: any[];
+	expressions: any[];
+};
+
+export type DataContractUpload = {
+	id: string;
+	abi: any;
+	contract_meta: any;
+	metadata: any;
+	slug: string;
+};
+
+export type DataAddressUpload = {
+	id: string;
+	address: string;
+	chain_id: number;
+	contract: string;
+	type: string;
+	implementation?: string;
+};
+
+export type DataDeployerUpload = {
+	id: string;
+	bytecode_hash: string;
+	opmeta: any;
+	opmeta_bytes: string;
+};
+
+export type DataDeployerAddressUpload = {
+	id: string;
+	deployer: string;
+	address: string;
+	chain_id: number;
+};
+
+export type DataRainterpreterUpload = {
+	id: string;
+	bytecode_hash: string;
+};
+
+export type DataRainterpreterAddressUpload = {
+	id: string;
+	rainterpreter: string;
+	address: string;
+	chain_id: number;
+};
+
+export type DataRainterpreterStoreUpload = {
+	id: string;
+	bytecode_hash: string;
+};
+
+export type DataRainterpreterStoreAddressUpload = {
+	id: string;
+	rainterpreter_store: string;
+	address: string;
+	chain_id: number;
+};
