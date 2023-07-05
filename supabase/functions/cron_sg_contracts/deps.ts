bbb3c0483bd04d964289e503176720750e3f10d4
NanezX <victor.nanezj@gmail.com>
2023-07-04 22:45:51 -0400

    remove test function

--- supabase/functions/cron_sg_contracts/deps.ts
+++
@@ -1,8 +0,0 @@
-import { arrayify, hexlify, isBytesLike } from 'https://esm.sh/ethers@5.7.2/lib/utils';
-import { inflate } from 'https://deno.land/x/compress@v0.4.5/zlib/mod.ts';
-import cbor from 'https://esm.sh/v111/cbor@8.1.0/es2022/cbor.js';
-
-import { createClient as createDbClient } from 'https://esm.sh/@supabase/supabase-js@2.0.0-rc.12';
-import { createClient as createSgClient } from 'https://esm.sh/v111/@urql/core@3.2.0/es2022/core.js';
-
-export { arrayify, isBytesLike, hexlify, inflate, cbor, createDbClient, createSgClient };
--- supabase/functions/cron_sg_contracts/utils/uuid.ts
+++
@@ -1,9 +0,0 @@
-import { v5 as uuidv5 } from 'https://esm.sh/uuid@9.0.0';
-
-/**
- * @public
- * This namespace is arbitrary picked. It work as a seed to generate new UUIDs.
- */
-const UUIDnamespace = '0d91cfc8-9be0-4458-8f05-5bd5a5bc2fbb';
-
-export { UUIDnamespace, uuidv5 };
